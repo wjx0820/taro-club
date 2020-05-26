44686341b65c7044f540dfe9e10bf125c0f0e968
Jexxie <1989wjx@gmail.com>
2020-05-26 11:24:12 +0800

    第一个页面

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,28 +1,43 @@
-import Taro, { Component } from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
+import Taro, { useState } from '@tarojs/taro'
+import { View } from '@tarojs/components'
+import { PostCard, PostForm } from '../../components'
 import './index.scss'
 
-export default class Index extends Component {
+export default function Index() {
+  const [posts, setPosts] = useState([
+    {
+      title: '第1️⃣篇博客📜',
+      content: '这是第1️⃣篇博客的内容。',
+    },
+  ])
+  const [formTitle, setFormTitle] = useState('')
+  const [formContent, setFormContent] = useState('')
 
-  componentWillMount () { }
+  function handleSubmit(e) {
+    e.preventDefault()
 
-  componentDidMount () { }
-
-  componentWillUnmount () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
-
-  config = {
-    navigationBarTitleText: '首页'
+    const newPosts = posts.concat({ title: formTitle, content: formContent })
+    setPosts(newPosts)
+    setFormTitle('')
+    setFormContent('')
   }
 
-  render () {
-    return (
-      <View className='index'>
-        <Text>Hello world!</Text>
-      </View>
-    )
-  }
+  return (
+    <View className="index">
+      {posts.map((post, index) => (
+        <PostCard key={index} title={post.title} content={post.content} />
+      ))}
+      <PostForm
+        formTitle={formTitle}
+        formContent={formContent}
+        handleSubmit={e => handleSubmit(e)}
+        handleTitleInput={e => setFormTitle(e.target.value)}
+        handleContentInput={e => setFormContent(e.target.value)}
+      />
+    </View>
+  )
+}
+
+Index.config = {
+  navigationBarTitleText: '首页',
 }
